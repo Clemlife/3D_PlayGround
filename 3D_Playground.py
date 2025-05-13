aa27dd7416dc5aa2412367a8750d5452c7e60ae6
clemlife <nuttallejoseph26@gmail.com>
2025-05-12 21:07:04 -0700

    Add movement for all shapes

--- 3D_Playground.py
+++ 3D_Playground.py
@@ -6,6 +6,7 @@ Let user modify and mess with 3d shaped using both mouse and keyboard controls
 """
 
 import pygame
+import copy
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
@@ -30,6 +31,15 @@ def draw_shape(edges, vertices):
             glVertex(vertices[vertex]) #Sets the vertexes from edges list
     glEnd() #Done making shapes
 
+def move_shape(vertice_list, mouse_x, mouse_y, back_vertice_list):
+
+
+    center = [(mouse_x-960)/220, (540-mouse_y)/160, 0]
+    print(center)
+    for i in range(len(vertice_list)):
+        for j in range(len(vertice_list[i])):
+
+            vertice_list[i][j] = center[j] + back_vertice_list[i][j]
 """
 GAME BUTTON CLASS
 """
@@ -88,10 +98,10 @@ def menu():
                     diamond_playground()
 
                 elif cube_button.rect.collidepoint(event.pos):  # Cube Selection
-                    print()
+                    cube_playground()
 
                 elif sphere_button.rect.collidepoint(event.pos):  # Sphere selection
-                    print()
+                    sphere_playground()
 
             # END FIND BUTTON PRESSED
 
@@ -133,7 +143,7 @@ def menu():
         """
         pygame.display.flip()
         clock.tick(60)
-
+###DIAMOND MODE###
 def diamond_playground():
     """
     Displays the full playground game mode for the diamond
@@ -154,6 +164,14 @@ def diamond_playground():
         [0, .5, 0],
         [0, -1.5, 0],
     ]
+    backup_vertices_diamond = [
+        [0.5, -0.5, 0.5],
+        [0.5, -0.5, -0.5],
+        [-0.5, -0.5, -0.5],
+        [-0.5, -0.5, 0.5],
+        [0, .5, 0],
+        [0, -1.5, 0],
+    ]
     # EDGES OF DIAMOND (each number isa  vertex)
     edges_diamond = [
         (0, 1), (1, 2), (2, 3), (3, 0),  # Center Square
@@ -162,20 +180,26 @@ def diamond_playground():
     ]
     ###END SHAPE CHARACTERISTICS###
 
-    clock = pygame.time.Clock()
+
     scale = 1 #No Scaling Base Variable
-    rotate = 1 # Slow Rotation Base variable (CHANGE THIS TO NO ROTATION?)
+    rotate = 0 # Slow Rotation Base variable (CHANGE THIS TO NO ROTATION?)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                draw_shape(edges_diamond, vertices_diamond)
+                x, y = pygame.mouse.get_pos()
+                move_shape(vertices_diamond, x, y, backup_vertices_diamond)
+
         if pygame.key.get_pressed()[K_1]:
             scale += .001
         if pygame.key.get_pressed()[K_2]:
             scale -= .001
         if pygame.key.get_pressed()[K_RETURN]:
             scale = 1
+            rotate = 0
         if pygame.key.get_pressed()[K_3]:
             rotate += .1
         if pygame.key.get_pressed()[K_4]:
@@ -188,7 +212,146 @@ def diamond_playground():
         draw_shape(edges_diamond, vertices_diamond)
         pygame.display.flip()
         clock.tick(60)
+###CUBE MODE###
+def cube_playground():
+    """
+    Displays the full playground game mode for the cube
+    """
+    ###3D INITIALIZING###
+    pygame.display.set_mode((WIDTH, HEIGHT), DOUBLEBUF|OPENGL) #Double buffering visuals and enable OpenGl to run in pygame window
 
+    gluPerspective(45, ((WIDTH, HEIGHT)[0]/(WIDTH, HEIGHT)[1]), 0.1, 50.0)
+    glTranslatef(0.0, 0.0, -5)
+
+    ###SHAPE CHARACTERISTICS###
+    # VERTICES OF CUBE
+    vertices_cube = [
+        [0.5, -0.5, 0.5],
+        [0.5, -0.5, -0.5],
+        [-0.5, -0.5, -0.5],
+        [-0.5, -0.5, 0.5],
+        [0.5, 0.5, 0.5],
+        [0.5, 0.5, -0.5],
+        [-0.5, 0.5, -0.5],
+        [-0.5, 0.5, 0.5],
+
+    ]
+    backup_vertices_cube= [
+        [0.5, -0.5, 0.5],
+        [0.5, -0.5, -0.5],
+        [-0.5, -0.5, -0.5],
+        [-0.5, -0.5, 0.5],
+        [0.5, 0.5, 0.5],
+        [0.5, 0.5, -0.5],
+        [-0.5, 0.5, -0.5],
+        [-0.5, 0.5, 0.5],
+
+    ]
+    # EDGES OF CUBE (each number isa  vertex)
+    edges_cube = [
+        (0, 1), (1, 2), (2, 3), (3, 0),  # Bottom Square
+        (4, 5), (5, 6), (6, 7), (7, 4),  #Top Square
+        (0, 4), (1, 5), (2, 6), (3, 7)  #Connect Squares
+    ]
+    ###END SHAPE CHARACTERISTICS###
+
+
+    scale = 1 #No Scaling Base Variable
+    rotate = 0 # Slow Rotation Base variable (CHANGE THIS TO NO ROTATION?)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                draw_shape(edges_cube, vertices_cube)
+                x, y = pygame.mouse.get_pos()
+                move_shape(vertices_cube, x, y, backup_vertices_cube)
+        if pygame.key.get_pressed()[K_1]:
+            scale += .001
+        if pygame.key.get_pressed()[K_2]:
+            scale -= .001
+        if pygame.key.get_pressed()[K_RETURN]:
+            scale = 1
+            rotate = 0
+        if pygame.key.get_pressed()[K_3]:
+            rotate += .1
+        if pygame.key.get_pressed()[K_4]:
+            rotate -= .1
+
+
+        glRotatef(rotate, 0, 1, 0)
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        glScalef(scale, scale, scale)
+        draw_shape(edges_cube, vertices_cube)
+        pygame.display.flip()
+        clock.tick(60)
+
+    ###SPHERE MODE###
+def sphere_playground():
+    """
+    Displays the full playground game mode for the cube
+    """
+    ###3D INITIALIZING###
+    pygame.display.set_mode((WIDTH, HEIGHT),DOUBLEBUF | OPENGL)  # Double buffering visuals and enable OpenGl to run in pygame window
+
+    gluPerspective(45, ((WIDTH, HEIGHT)[0] / (WIDTH, HEIGHT)[1]), 0.1, 50.0)
+    glTranslatef(0.0, 0.0, -5)
+
+    quadrie = gluNewQuadric()
+    gluQuadricDrawStyle(quadrie, GLU_FILL)
+    gluQuadricNormals(quadrie, GL_SMOOTH)
+
+
+    scale = 1  # No Scaling Base Variable
+    rotate = 0  # Slow Rotation Base variable (CHANGE THIS TO NO ROTATION?)
+    numerate = 0
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            ### DIFFICULT MOVEMENT FOR SPHERE###
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                file_xy_read = open("store_x_y", "r")
+                file_xy_write = open("store_x_y", "w")
+                numerate += 1
+                old_x, old_y = 960, 540
+                x, y = pygame.mouse.get_pos()
+                print(x, y)
+                if numerate >= 2:
+                    string1, string2 = file_xy_read.readlines()
+                    old_x, old_y = float(string1), float(string2)
+                    print(old_x, old_y)
+                    ex, why = ((old_x-960)/460), ((old_y-540)/300)
+
+                #FIND DIFFERENCE BETWEEN CURRENT COORDINATES AND PAST COORDINATES TO DECIDE TRANSLATION DISTANCE THAT FITS CURSOR
+                glTranslatef(x/460 - old_x/460, -y/300 - -old_y/300, 0)
+                #SAVE COORDINATES BEFORE
+                file_xy_write.write(str(x))
+                file_xy_write.write("\n" + str(y))
+
+        if pygame.key.get_pressed()[K_1]:
+            scale += .001
+        if pygame.key.get_pressed()[K_2]:
+            scale -= .001
+        if pygame.key.get_pressed()[K_RETURN]:
+            scale = 1
+            rotate = 0
+        if pygame.key.get_pressed()[K_3]:
+            rotate += .1
+        if pygame.key.get_pressed()[K_4]:
+            rotate -= .1
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glColor3f(0.2, 0.01, .01)
+        #SPHERE
+        gluSphere(quadrie, 1, 32, 32)
+        glVertex(0, 0,1 * scale)
+        glRotatef(rotate, 0, 1, 0)
+        glScalef(scale, scale, scale)
+        pygame.display.flip()
+        clock.tick(60) 
 def main():
     menu()
 
